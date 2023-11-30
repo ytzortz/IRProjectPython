e22208af15305588dbbabe9567c3bb15eaa9cf9c
John Tzortzakis <103953727+JohnTzortz@users.noreply.github.com>
2023-11-30 12:05:22 +0100

    HTML improved

--- genres.py
+++ genres.py
@@ -1,23 +1,20 @@
 import csv
 
-csv_file_path = "databases/mergedDatabase.csv"  # Replace with the path to your CSV file
+# Replace 'your_file.csv' with the actual path to your CSV file
+csv_file_path = 'databases/mergedDatabase.csv'
 
-# Set to store unique genres
+# Create an empty set to store unique genres
 unique_genres = set()
 
+# Open the CSV file and read the genres from each row
 with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile)
-    header = next(reader)
+    header = next(reader)  # Assuming the first row is the header
 
     for row in reader:
-        # Genres are in the 3rd column (index 2)
-        genres = row[2]
+        genres = row[2]  # Assuming the genres are in the third column, adjust if needed
+        genres_list = eval(genres)  # Convert the string representation of a list to an actual list
+        unique_genres.update(str(genre) for genre in genres_list)
 
-        # Assuming genres are stored in a JSON-like format in the CSV, you can use eval to convert it to a list
-        genres_list = eval(genres)
-
-        # Update the set of unique genres
-        unique_genres.update(genres_list)
-
-# Print the list of unique genres
-print("Unique Genres:", list(unique_genres))
+# Print the unique genres
+print(list(unique_genres))
